999b6f861d60ee29bfd2bc6b823188827a2a4415
Jaime Pangan <jaimepangn@gmail.com>
2021-07-20 09:42:50 +0800

    Second commit, added code

--- script.js
+++ script.js
@@ -1,2 +1,15 @@
 //excuses generator
 
+const excuses = [
+    'I have food poisoning',
+    'I already made other commitments',
+    'I have no money',
+    "My mum didn't allow me"
+]
+
+let randomExcuse = excuses[Math.floor(Math.random() * 4)];
+
+console.log (randomExcuse);
+
+//Test
+//console.log (excuses);
