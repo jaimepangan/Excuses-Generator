483dbac545a58203b81d1d19092db2933bd695fe
Jaime Pangan <jaimepangn@gmail.com>
2021-07-20 10:00:05 +0800

    Third commit, created a function for the code

--- script.js
+++ script.js
@@ -4,12 +4,21 @@ const excuses = [
     'I have food poisoning',
     'I already made other commitments',
     'I have no money',
-    "My mum didn't allow me"
+    "My mum didn't allow me",
+    "You're not cool"
 ]
 
-let randomExcuse = excuses[Math.floor(Math.random() * 4)];
+//Creating a function for the code so we can pass in multiple arrays
+const excuseGenerator = arr => {
+    let ranExcuse = arr[Math.floor(Math.random() * arr.length)];
+    return ranExcuse;
+}
 
-console.log (randomExcuse);
+//Testing the function
+console.log (excuseGenerator(excuses));
 
-//Test
-//console.log (excuses);
+
+
+
+//Testing the generator code
+//let randomExcuse = excuses[Math.floor(Math.random() * 4)];
